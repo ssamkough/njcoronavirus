96bed07807e3b7ecaa0a58b0549f4c0d338bfc2b
Sammy Samkough <sammysamkough@gmail.com>
2020-03-16 17:41:26 -0400

    Added phone number for people to call

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,19 @@
 <template>
   <v-container>
     <v-row>
+      <v-col>
+        <blockquote class="custom-blockquote">
+          Have general questions about COVID-19?
+          <span class="font-weight-bold">Call the NJ COVID-19</span> &
+          Poison Center Public Hotline at
+          <span class="font-weight-bold">
+            <a href="tel:18009621253">1-800-962-1253</a>
+          </span> or
+          <a href="tel:18002221222">1-800-222-1222</a>!
+        </blockquote>
+      </v-col>
+    </v-row>
+    <v-row>
       <v-col md="4">
         <span>Last Updated: {{date}}</span>
       </v-col>
