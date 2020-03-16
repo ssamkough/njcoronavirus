b85972a316141b4e8ecb6d84196e63f6ae7f819f
Sammy Samkough <sammysamkough@gmail.com>
2020-03-16 17:39:44 -0400

    Added header

---
+++ src/components/Header.vue
@@ -0,0 +1,22 @@
+<template>
+  <nav class="nav-toolbar">
+    <v-toolbar flat app class="grey lighten-2">
+      <v-toolbar-title>
+        <span class="headline font-weight-black">New Jersey Covid-19</span>
+      </v-toolbar-title>
+    </v-toolbar>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "Header"
+};
+</script>
+
+<style scoped>
+.nav-toolbar {
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
