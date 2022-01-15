d8332aa56f67d7b758dc4f9fcb39231755d32d60
Sammy <sammysamkough@hotmail.com>
2022-01-15 06:29:48 -0800

    made announcement more clear

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="maps">
+    <span class="announcement">
     Website has ceased operations since May 2021. For more information regarding the Coronavirus/Covid-19, please visit the <a
-            href="https://www.cdc.gov/coronavirus/2019-ncov/index.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >Centers for Disease Control and Prevention</a>.
-    <br />
+        href="https://www.cdc.gov/coronavirus/2019-ncov/index.html"
+        target="_blank"
+        rel="noopener noreferrer"
+    >Centers for Disease Control and Prevention</a>.
+    </span>
     <MainMap />
-    <br />
     <DayByDayMap />
   </div>
 </template>
@@ -24,3 +24,17 @@ export default {
   }
 };
 </script>
+
+<style>
+.maps {
+  display: flex;
+  flex-direction: column;
+  gap: 30px;
+}
+
+.announcement {
+  color: red;
+  font-weight: bold;
+  font-size: 1.2em;
+}
+</style>
